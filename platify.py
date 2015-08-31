e010c74aa9ef3a2cc546c59ce609677cd6b19405
David Wegmuller <david@wegmuller.org>
2015-08-30 19:40:44 -0700

    all but inventory generation working

--- platify.py
+++ platify.py
@@ -1,43 +1,26 @@
 import png
 import sys
+from colorama import init, Fore
+def fit_part(array, x, y, length, maxWidth):
+    count = 0
+    while count < length :
+        if (x + count) < maxWidth :
+            if array[y][x] != array[y][x + count] :
+                return 0
+        else :
+            return 0
+        count = count + 1
+    return 1
 
-def draw8():
-    sys.stdout.write('P------|')
-    sys.stdout.write('|   8  |')
-    sys.stdout.write('L______|')
-
-def draw6():
-    sys.stdout.write('P----|')
-    sys.stdout.write('| 8  |')
-    sys.stdout.write('L____|')
-
-def draw4():
-    sys.stdout.write('P--|')
-    sys.stdout.write('|8 |')
-    sys.stdout.write('L__|')
-
-def draw3():
-    sys.stdout.write('P-|')
-    sys.stdout.write('|8|')
-    sys.stdout.write('L_|')
-
-def draw2():
-    sys.stdout.write('22')
-    sys.stdout.write('22')
-    sys.stdout.write('22')
-    
-def draw1():
-    sys.stdout.write('1')
-    sys.stdout.write('1')
-    sys.stdout.write('1')
-
+# the source png file must have dimensions that are multiples of 5 for x and multiples of 2 for y.
 r=png.Reader('logo_18.png')
+# height is in plates
 height = r.read()[1]
 width = r.read()[0]
 print "width =", width, "height =", height
 print r.read()
 l=list(r.read()[2])
-
+# this image will be the plated version of original: note the aspect ratio is off (squeezed sideways).
 f = open('plates.png', 'wb')
 w = png.Writer(width / 5, height / 2, greyscale=True)
 output = [None] *(height / 2)
@@ -48,6 +31,7 @@ while i < height :
     bottomLine = l[i + 1]
     outputRow = [None]*(width / 5)
     while j < (width * 3) :
+        # use a majority to decide if a given plate (made of 10 pixels) should be black or white.
         if (topLine[j] + topLine[j + 3] + topLine[j + 6] + topLine[j + 9] + topLine[j + 12] + bottomLine[j] + bottomLine[j + 3] + bottomLine[j + 6] + bottomLine[j + 9] + bottomLine[j + 12]) < (10 * 255) :
             sys.stdout.write('#')
             outputRow[int(j / 15)] = 0
@@ -66,6 +50,7 @@ i = 0
 print " "
 print " "
 print " "
+# This file uses four colors: black for black plates, dark grey for black bricks, white for white plates, light grey for white bricks.
 f = open('platesNbricks.png', 'wb')
 w = png.Writer(width / 5, height / 2, greyscale=True)
 
@@ -86,6 +71,7 @@ while i < (int(height / 2) - 2) :
 w.write(f, output)
 f.close()
 
+# Display the logo using characters to show lines of plates versus bricks
 i = 0.0
 while i < int(height / 2) :
     j = 0
@@ -93,6 +79,7 @@ while i < int(height / 2) :
         if output[int(i)][j] == 0 :
             sys.stdout.write('#')
         if output[int(i)][j] == 64 :
+            # bricks are made of three rows.
             if (i / 3) == int (i / 3) :
                 sys.stdout.write('T')
             else :
@@ -102,6 +89,7 @@ while i < int(height / 2) :
                     sys.stdout.write('=')
                 
         if output[int(i)][j] == 192 :
+            # bricks are made of three rows.
             if (i / 3) == int (i / 3) :
                 sys.stdout.write('.')
             else :
@@ -114,3 +102,77 @@ while i < int(height / 2) :
         j = j + 1
     print " "
     i = i + 1.0            
+
+# split rows into bricks or plates. Available lengths are 8, 6, 4, 3, 2 and 1.
+
+print " "
+print " "
+print " "
+sizesOfBricks = [8, 6, 4, 3, 2, 1]
+whiteplateStrings = ["8======8","6====6","4==4","3=3","==","~"]
+blackplateStrings = ["88888888","666666","4444","333","__","."]
+whitetopBrickStrings = ["I------I","I----I","I--I","I-I","--","I"]
+whitemedBrickStrings = ["I 8888 I","I 66 I","I44I","I3I","II","I"]
+whitebotBrickStrings = ["I______I","I____I","I__I","I_I","__","I"]
+blacktopBrickStrings = ["|------|","|----|","|--|","|-|","--","1"]
+blackmedBrickStrings = ["| 8888 |","| 66 |","|44|","|3|","22","1"]
+blackbotBrickStrings = ["|______|","|____|","|__|","|_|","__","1"]
+i = 0.0
+while i < int(height / 2) :
+    j = 0
+    while j < int(width / 5) :
+        if output[int(i)][j] == 0 :
+            # Black plates
+            sCnt = 0
+            while sCnt < len(sizesOfBricks) and j < int(width / 5) and output[int(i)][j] == 0:
+                if 1 == fit_part(output, j, int(i), sizesOfBricks[sCnt], int(width / 5)) :
+                    j = j + sizesOfBricks[sCnt]
+                    sys.stdout.write(Fore.RED + blackplateStrings[sCnt])
+                else :
+                    sCnt = sCnt + 1
+        else :
+            if output[int(i)][j] == 64 :
+                # Black bricks
+                sCnt = 0
+                while sCnt < len(sizesOfBricks) and j < int(width / 5) and output[int(i)][j] == 64:
+                    if 1 == fit_part(output, j, int(i), sizesOfBricks[sCnt], int(width / 5)) :
+                        j = j + sizesOfBricks[sCnt]
+                        if (i / 3) == int (i / 3) :
+                            sys.stdout.write(Fore.RED + blacktopBrickStrings[sCnt])
+                        else :
+                            if ((i - 1) / 3) == int((i - 1) / 3) :
+                                sys.stdout.write(Fore.RED + blackmedBrickStrings[sCnt])
+                            else :
+                                sys.stdout.write(Fore.RED + blackbotBrickStrings[sCnt])
+                    else :
+                        sCnt = sCnt + 1
+            else :                
+                if output[int(i)][j] == 192 :
+                    # White bricks
+                    sCnt = 0
+                    while sCnt < len(sizesOfBricks) and j < int(width / 5) and output[int(i)][j] == 192:
+                        if 1 == fit_part(output, j, int(i), sizesOfBricks[sCnt], int(width / 5)) :
+                            j = j + sizesOfBricks[sCnt]
+                            if (i / 3) == int (i / 3) :
+                                sys.stdout.write(Fore.WHITE + whitetopBrickStrings[sCnt])
+                            else :
+                                if ((i - 1) / 3) == int((i - 1) / 3) :
+                                    sys.stdout.write(Fore.WHITE + whitemedBrickStrings[sCnt])
+                                else :
+                                    sys.stdout.write(Fore.WHITE + whitebotBrickStrings[sCnt])
+                        else :
+                            sCnt = sCnt + 1
+                else :
+                    if output[int(i)][j] == 255 :
+                        # white plates
+                        sCnt = 0
+                        while sCnt < len(sizesOfBricks) and j < int(width / 5) and output[int(i)][j] == 255:
+                            if 1 == fit_part(output, j, int(i), sizesOfBricks[sCnt], int(width / 5)) :
+                                j = j + sizesOfBricks[sCnt]
+                                sys.stdout.write(Fore.WHITE + whiteplateStrings[sCnt])
+                            else :
+                                sCnt = sCnt + 1
+    print " "
+    i = i + 1.0     
+print int(width / 5), int(height /2)
+       
